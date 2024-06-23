e7905957608acc61bd8459f66f405b23b800fe81
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-06-23 18:51:01 +0100

    Split Text Into Fragments
    Use RecursiveCharacterTextSplitter to split text into fragments

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 
 
@@ -8,3 +9,7 @@ load_dotenv()
 # Use WebBaseLoader to extract data from website
 web_loader = WebBaseLoader("https://www.dasa.org")
 web_text = web_loader.load()
+
+# Use RecursiveCharacterTextSplitter to split text into fragments
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(documents=web_text)
