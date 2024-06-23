f4185d1008ebe17e46aa641a8ff1be2074ecc61b
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-06-23 18:52:20 +0100

    Get Vector Embeddings Algorithm
    Get vector embeddings algorithm from HuggingFaceEmbeddings

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface.embeddings.huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 
 
@@ -13,3 +14,13 @@ web_text = web_loader.load()
 # Use RecursiveCharacterTextSplitter to split text into fragments
 text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(documents=web_text)
+
+# Convert fragmented text into vector embeddings
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embeddings = HuggingFaceEmbeddings(
+    model_name = model_name,
+    model_kwargs = model_kwargs,
+    encode_kwargs = encode_kwargs
+)
