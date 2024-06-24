51a6ef77a4b3663c55f723aa829bf4850d5921c0
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-06-24 22:30:59 +0100

    Embeddings and Environment Variables
    Load HuggingFaceHub API key, and get Embedding model

--- conversation.py
+++ conversation.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+load_dotenv()
 
 # Extract data from website
 web_loader = WebBaseLoader("https://www.dasa.org")
@@ -9,3 +12,8 @@ web_text = web_loader.load()
 # Split text into fragments
 text_splitter = RecursiveCharacterTextSplitter()
 web_fragments = text_splitter.split_text(web_text)
+
+# Get Embedding model/algorithms/function
+embeddings = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-mpnet-base-v2"
+)
