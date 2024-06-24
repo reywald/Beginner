c7d6e625c000954597688a4214a58ca82a072970
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-06-24 22:36:10 +0100

    Vector Store and Retriever
    Instantiate a VectorStore using the fragments and embeddings model and create a Retriever

--- conversation.py
+++ conversation.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -11,9 +12,14 @@ web_text = web_loader.load()
 
 # Split text into fragments
 text_splitter = RecursiveCharacterTextSplitter()
-web_fragments = text_splitter.split_text(web_text)
+web_documents = text_splitter.split_documents(web_text)
 
 # Get Embedding model/algorithms/function
 embeddings = HuggingFaceEndpointEmbeddings(
     model="sentence-transformers/all-mpnet-base-v2"
 )
+
+# Instantiate Vector Store and its Retriever
+vector_store = FAISS.from_documents(
+    documents=web_documents, embedding=embeddings)
+retriever = vector_store.as_retriever()
