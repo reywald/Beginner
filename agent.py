f35fd7ab8b383736a59b2eeb6896743b3d7e72d6
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-07-17 16:25:29 +0100

    Split Text Into Document Fragments

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Use WebBaseLoader to extract data from the dasa.org website
@@ -8,3 +9,9 @@ web_contents = web_loader.load()
 # Use RecursivCharacterTextSplitter to split text into document fragments
 text_splitter = RecursiveCharacterTextSplitter()
 document_fragments = text_splitter.split_documents(documents=web_contents)
+
+# Get vector embedding function/model
+model_name = "sentence-transformers/all-mpnet-base-v2"
+embeddings = HuggingFaceEndpointEmbeddings(
+    model=model_name,
+)
