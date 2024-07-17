ce690f5f88a5551d812010551e0c3cf91bd3a45d
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-07-17 22:14:56 +0100

    Add Web Search Tool
    Add the Tavily API key and import the TavilySearchResults tool

--- agent.py
+++ agent.py
@@ -1,8 +1,13 @@
+from dotenv import load_dotenv
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.tools.retriever import create_retriever_tool
+
+# Load the HuggingFace API Token and the Tavily API Key
+load_dotenv()
 
 # Use WebBaseLoader to extract data from the dasa.org website
 web_loader = WebBaseLoader("https://www.dasa.org")
@@ -25,7 +30,13 @@ retriever = vector_store.as_retriever()
 
 # Create retriever tool
 retriever_tool = create_retriever_tool(
-    retriever=retriever, 
+    retriever=retriever,
     name="DASA_search",
     description="Search for information about DASA. For any questions about DASA, you must use this tool"
 )
+
+# Create tool used for searching the internet
+search_tool = TavilySearchResults()
+
+# Create list of prepared AI tools
+tools = [retriever_tool, search_tool]
