f15e635f6e19569e59dd0237a45fe01505b7dbac
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-07-17 16:29:29 +0100

    Create Vector Store amd Retriever

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
 from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -15,3 +16,8 @@ model_name = "sentence-transformers/all-mpnet-base-v2"
 embeddings = HuggingFaceEndpointEmbeddings(
     model=model_name,
 )
+
+# Create Vector Store, initialized from documents and embedding algorithm
+vector_store = FAISS.from_documents(
+    documents=document_fragments, embedding=embeddings)
+retriever = vector_store.as_retriever()
